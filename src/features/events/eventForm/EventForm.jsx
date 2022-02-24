bc447d019cf78b385422d247429024be83cfbb0b
Eyüp Demir <eyupdemir@gmail.com>
2022-02-24 13:00:05 +0300

    Google map added to event detail

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,3 +1,4 @@
+/* global google */
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { Button, Header, Segment } from 'semantic-ui-react';
@@ -11,6 +12,7 @@ import MyTextArea from '../../../app/common/form/MyTextArea';
 import MySelectInput from '../../../app/common/form/MySelectInput';
 import { categoryData } from '../../../app/api/categoryOptions';
 import MyDateInput from '../../../app/common/form/MyDateInput';
+import MyPlaceInput from '../../../app/common/form/MyPlaceInput';
 
 const EventForm = () => {
   const navigate = useNavigate();
@@ -24,8 +26,14 @@ const EventForm = () => {
     title: '',
     category: '',
     description: '',
-    city: '',
-    venue: '',
+    city: {
+      address: '',
+      latLng: null,
+    },
+    venue: {
+      address: '',
+      latLng: null,
+    },
     date: '',
   };
 
@@ -33,8 +41,12 @@ const EventForm = () => {
     title: Yup.string().required('You must provide a title!'),
     category: Yup.string().required('You must provide a category!'),
     description: Yup.string().required(),
-    city: Yup.string().required(),
-    venue: Yup.string().required(),
+    city: Yup.object().shape({
+      address: Yup.string().required('City is required'),
+    }),
+    venue: Yup.object().shape({
+      address: Yup.string().required('Venue is required'),
+    }),
     date: Yup.string().required(),
   });
 
@@ -58,7 +70,7 @@ const EventForm = () => {
           navigate('/events');
         }}
       >
-        {({ isSubmitting, dirty, isValid }) => (
+        {({ isSubmitting, dirty, isValid, values }) => (
           <Form className='ui form'>
             <Header sub color='teal' content='Event Details' />
             <MyTextInput name='title' placeholder='Event title' />
@@ -69,8 +81,17 @@ const EventForm = () => {
             />
             <MyTextArea name='description' placeholder='Description' rows={3} />
             <Header sub color='teal' content='Event Location Details' />
-            <MyTextInput name='city' placeholder='City' />
-            <MyTextInput name='venue' placeholder='Venue' />
+            <MyPlaceInput name='city' placeholder='City' />
+            <MyPlaceInput
+              name='venue'
+              disabled={!values.city.latLng}
+              placeholder='Venue'
+              options={{
+                location: new google.maps.LatLng(values.city.latLng),
+                radius: 1000,
+                types: ['establishment'],
+              }}
+            />
             <MyDateInput
               name='date'
               placeholderText='Date'
--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { Button } from 'semantic-ui-react';
 import { openModal } from '../../app/common/modals/modalReducer';
+import TestMap from './TestMap';
+import TestPlaceInput from './TestPlaceInput';
 
 import { increment, decrement } from './testReducer';
 
 const Sandbox = () => {
   const data = useSelector((state) => state.test.data);
   const dispatch = useDispatch();
+
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
+  const [location, setLocation] = useState(defaultProps);
+
+  function handleSetLocation(latLng) {
+    setLocation({ ...location, center: latLng });
+  }
 
   return (
     <>
@@ -31,6 +46,10 @@ const Sandbox = () => {
         content='Open Modal'
         color='teal'
       />
+      <div style={{ marginTop: 15 }}>
+        <TestPlaceInput setLocation={handleSetLocation} />
+        <TestMap location={location} />
+      </div>
     </>
   );
 };
